9cf201761e1ab382f97355707f109aaa0d1db4d4
carlos <carlos@macbookpro-1.home>
2026-06-23 12:12:23 +0200

    add 3M

--- app/notification.py
+++ app/notification.py
@@ -47,7 +47,7 @@ class Notifier():
                    "bitget":"bitget"
                   }
 
-    periodMap = {"15min":"15分钟","30min":"30分钟","1h":"1小时", "1d":"日线", "d":"日线", "3d":"3日", "4h":"4小时", "6h":"6小时", "12h":"12小时", "w":"周线", "M":"月线"}
+    periodMap = {"15min":"15分钟","30min":"30分钟","1h":"1小时", "1d":"日线", "d":"日线", "3d":"3日", "4h":"4小时", "6h":"6小时", "12h":"12小时", "w":"周线", "M":"月线", "3M":"3月"}
     cst_tz = timezone('Asia/Shanghai')
     utc_tz = timezone('UTC')
     webhook = 'https://oapi.dingtalk.com/robot/send?access_token=1c46cb23a6562a4325bea9cf4225b11209e4b2fc4edd7d07e2ef404e15a86f0b'
